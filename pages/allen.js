135b7c57229c4d0d83074c82fdb661e7cb76da02
Andrew Maddocks <andrewmadd@icloud.com>
2019-12-19 15:26:15 -0700

    added things

---
+++ pages/allen.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Allen() {
+  return (
+    <div>
+      <Link href="/andrew">/andrew</Link>
+      <h1>allen</h1>
+    </div>
+  );
+}
---
+++ pages/andrew.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Andrew() {
+  return (
+    <div>
+      <Link href="/allen">/allen</Link>
+      <h1>andrew</h1>
+    </div>
+  );
+}
